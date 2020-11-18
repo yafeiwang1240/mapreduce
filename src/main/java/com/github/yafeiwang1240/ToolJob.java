00530f82d8b751a85fea996c5757fad3eecbbe97
wangyafei <364968487@qq.com>
2020-11-18 18:05:07 +0800

    ToolRun

--- src/main/java/com/github/yafeiwang1240/ToolJob.java
+++ src/main/java/com/github/yafeiwang1240/ToolJob.java
@@ -7,7 +7,6 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobContext;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
